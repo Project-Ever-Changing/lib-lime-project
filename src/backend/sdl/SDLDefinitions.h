87b523e2beeb360f68c4e0becef5ccb7aba3d6e0
Diego Fonseca <58647349+Just-Feeshy@users.noreply.github.com>
2023-05-01 18:06:14 -0400

    Update SDLDefinitions.h

--- src/backend/sdl/SDLDefinitions.h
+++ src/backend/sdl/SDLDefinitions.h
@@ -3,7 +3,7 @@
 #define SDL_CreateContext SDL_Metal_CreateView
 #define SDL_DestroyContext SDL_Metal_DestroyView
 #define SDL_GetDrawableSize SDL_Metal_GetDrawableSize
-#define SDL_MetalView SDL_Context
+#define SDL_Context SDL_MetalView
 #define SDL_SetAttribute EMPTY
 #define SDL_MakeCurrent EMPTY
 #define SDL_SwapWindow EMPTY
@@ -14,7 +14,7 @@
 #define SDL_CreateContext SDL_GL_CreateContext
 #define SDL_DestroyContext SDL_GL_DestroyContext
 #define SDL_GetDrawableSize SDL_GL_GetDrawableSize
-#define SDL_GLContext SDL_Context
+#define SDL_Context SDL_GLContext
 #define SDL_SetAttribute SDL_GL_SetAttribute
 #define SDL_MakeCurrent SDL_GL_MakeCurrent
 #define SDL_SwapWindow SDL_GL_SwapWindow
