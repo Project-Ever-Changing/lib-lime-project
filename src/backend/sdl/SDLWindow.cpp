59955306f92cc5b47d11d28d978658b787b7f112
Diego Fonseca <58647349+Just-Feeshy@users.noreply.github.com>
2023-05-02 10:46:20 -0400

    Make the method more diverse

--- src/backend/sdl/SDLWindow.cpp
+++ src/backend/sdl/SDLWindow.cpp
@@ -88,60 +88,60 @@ namespace lime {
             }
 
 #if defined (HX_WINDOWS) && defined (NATIVE_TOOLKIT_SDL_ANGLE)
-            SDL_GL_SetAttribute (SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
-			SDL_GL_SetAttribute (SDL_GL_CONTEXT_MAJOR_VERSION, 2);
-			SDL_GL_SetAttribute (SDL_GL_CONTEXT_MINOR_VERSION, 0);
+            SDL_SetAttribute (SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
+			SDL_SetAttribute (SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+			SDL_SetAttribute (SDL_GL_CONTEXT_MINOR_VERSION, 0);
 			SDL_SetHint (SDL_HINT_VIDEO_WIN_D3DCOMPILER, "d3dcompiler_47.dll");
 #endif
 
 #if defined (RASPBERRYPI)
-            SDL_GL_SetAttribute (SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
-			SDL_GL_SetAttribute (SDL_GL_CONTEXT_MAJOR_VERSION, 2);
-			SDL_GL_SetAttribute (SDL_GL_CONTEXT_MINOR_VERSION, 0);
+            SDL_SetAttribute (SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
+			SDL_SetAttribute (SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+			SDL_SetAttribute (SDL_GL_CONTEXT_MINOR_VERSION, 0);
 			SDL_SetHint (SDL_HINT_RENDER_DRIVER, "opengles2");
 #endif
 
 #if defined (IPHONE) || defined (APPLETV)
-            SDL_GL_SetAttribute (SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
-			SDL_GL_SetAttribute (SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+            SDL_SetAttribute (SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
+			SDL_SetAttribute (SDL_GL_CONTEXT_MAJOR_VERSION, 3);
 #endif
 
             if (flags & WINDOW_FLAG_DEPTH_BUFFER) {
 
-                SDL_GL_SetAttribute (SDL_GL_DEPTH_SIZE, 32 - (flags & WINDOW_FLAG_STENCIL_BUFFER) ? 8 : 0);
+                SDL_SetAttribute (SDL_GL_DEPTH_SIZE, 32 - (flags & WINDOW_FLAG_STENCIL_BUFFER) ? 8 : 0);
 
             }
 
             if (flags & WINDOW_FLAG_STENCIL_BUFFER) {
 
-                SDL_GL_SetAttribute (SDL_GL_STENCIL_SIZE, 8);
+                SDL_SetAttribute (SDL_GL_STENCIL_SIZE, 8);
 
             }
 
             if (flags & WINDOW_FLAG_HW_AA_HIRES) {
 
-                SDL_GL_SetAttribute (SDL_GL_MULTISAMPLEBUFFERS, true);
-                SDL_GL_SetAttribute (SDL_GL_MULTISAMPLESAMPLES, 4);
+                SDL_SetAttribute (SDL_GL_MULTISAMPLEBUFFERS, true);
+                SDL_SetAttribute (SDL_GL_MULTISAMPLESAMPLES, 4);
 
             } else if (flags & WINDOW_FLAG_HW_AA) {
 
-                SDL_GL_SetAttribute (SDL_GL_MULTISAMPLEBUFFERS, true);
-                SDL_GL_SetAttribute (SDL_GL_MULTISAMPLESAMPLES, 2);
+                SDL_SetAttribute (SDL_GL_MULTISAMPLEBUFFERS, true);
+                SDL_SetAttribute (SDL_GL_MULTISAMPLESAMPLES, 2);
 
             }
 
             if (flags & WINDOW_FLAG_COLOR_DEPTH_32_BIT) {
 
-                SDL_GL_SetAttribute (SDL_GL_RED_SIZE, 8);
-                SDL_GL_SetAttribute (SDL_GL_GREEN_SIZE, 8);
-                SDL_GL_SetAttribute (SDL_GL_BLUE_SIZE, 8);
-                SDL_GL_SetAttribute (SDL_GL_ALPHA_SIZE, 8);
+                SDL_SetAttribute (SDL_GL_RED_SIZE, 8);
+                SDL_SetAttribute (SDL_GL_GREEN_SIZE, 8);
+                SDL_SetAttribute (SDL_GL_BLUE_SIZE, 8);
+                SDL_SetAttribute (SDL_GL_ALPHA_SIZE, 8);
 
             } else {
 
-                SDL_GL_SetAttribute (SDL_GL_RED_SIZE, 5);
-                SDL_GL_SetAttribute (SDL_GL_GREEN_SIZE, 6);
-                SDL_GL_SetAttribute (SDL_GL_BLUE_SIZE, 5);
+                SDL_SetAttribute (SDL_GL_RED_SIZE, 5);
+                SDL_SetAttribute (SDL_GL_GREEN_SIZE, 6);
+                SDL_SetAttribute (SDL_GL_BLUE_SIZE, 5);
 
             }
 
@@ -150,10 +150,10 @@ namespace lime {
         sdlWindow = SDL_CreateWindow (title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, sdlWindowFlags);
 
 #if defined (IPHONE) || defined (APPLETV)
-        if (sdlWindow && !SDL_GL_CreateContext (sdlWindow)) {
+        if (sdlWindow && !SDL_CreateContext (sdlWindow)) {
 
 			SDL_DestroyWindow (sdlWindow);
-			SDL_GL_SetAttribute (SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+			SDL_SetAttribute (SDL_GL_CONTEXT_MAJOR_VERSION, 2);
 
 			sdlWindow = SDL_CreateWindow (title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, sdlWindowFlags);
 
@@ -213,17 +213,17 @@ namespace lime {
 
             // }
 
-            context = SDL_GL_CreateContext (sdlWindow);
+            context = SDL_CreateContext (sdlWindow);
 
-            if (context && SDL_GL_MakeCurrent (sdlWindow, context) == 0) {
+            if (context && SDL_MakeCurrent (sdlWindow, context) == 0) {
 
                 if (flags & WINDOW_FLAG_VSYNC) {
 
-                    SDL_GL_SetSwapInterval (1);
+                    SDL_SetSwapInterval (1);
 
                 } else {
 
-                    SDL_GL_SetSwapInterval (0);
+                    SDL_SetSwapInterval (0);
 
                 }
 
@@ -244,7 +244,7 @@ namespace lime {
 
                 if (version < 2 && !strstr ((const char*)glGetString (GL_VERSION), "OpenGL ES")) {
 
-                    SDL_GL_DeleteContext (context);
+                    SDL_DeleteContext (context);
                     context = 0;
 
                 }
@@ -262,7 +262,7 @@ namespace lime {
 
             } else {
 
-                SDL_GL_DeleteContext (context);
+                SDL_DeleteContext (context);
                 context = NULL;
 
             }
@@ -308,7 +308,7 @@ namespace lime {
 
         } else if (context) {
 
-            SDL_GL_DeleteContext (context);
+            SDL_DeleteContext (context);
 
         }
 
@@ -362,7 +362,7 @@ namespace lime {
 
         if (context && !sdlRenderer) {
 
-            SDL_GL_SwapWindow (sdlWindow);
+            SDL_SwapWindow (sdlWindow);
 
         } else if (sdlRenderer) {
 
@@ -462,7 +462,7 @@ namespace lime {
 
         if (sdlWindow && context) {
 
-            SDL_GL_MakeCurrent (sdlWindow, context);
+            SDL_MakeCurrent (sdlWindow, context);
 
         }
 
@@ -614,7 +614,7 @@ namespace lime {
             int outputWidth;
             int outputHeight;
 
-            SDL_GL_GetDrawableSize (sdlWindow, &outputWidth, &outputHeight);
+            SDL_GetDrawableSize (sdlWindow, &outputWidth, &outputHeight);
 
             int width;
             int height;
